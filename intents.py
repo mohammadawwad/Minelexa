ddb44f5366070ba14f11b3726e78673257c75842
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2021-08-19 05:20:59 +0000

    able to scrape data of site using beautiful soup now

--- intents.py
+++ intents.py
@@ -1,17 +1,38 @@
 import ask_sdk_core.utils as ask_utils
 from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
-from bs4 import BeautifulSoup
-
-
-# Add a new intent handler class here!
-# You must implement can_handle() and handle()
-
 import feedparser
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-# html = open('https://www.minecraftcraftingguide.net/').read()
-# soup = BeautifulSoup(html)
-# print(soup.find(id='boat'))
+URL = "https://www.minecraftcraftingguide.net"
+r = requests.get(URL)
+   
+soup = BeautifulSoup(r.content, 'html5lib')
+print(soup.prettify())
+   
+# quotes=[]  # a list to store quotes
+   
+# table = soup.find('div', attrs = {'id':'all_quotes'}) 
+   
+# for row in table.findAll('div',
+#                          attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
+#     quote = {}
+#     quote['theme'] = row.h5.text
+#     quote['url'] = row.a['href']
+#     quote['img'] = row.img['src']
+#     quote['lines'] = row.img['alt'].split(" #")[0]
+#     quote['author'] = row.img['alt'].split(" #")[1]
+#     quotes.append(quote)
+   
+# filename = 'inspirational_quotes.csv'
+# with open(filename, 'w', newline='') as f:
+#     w = csv.DictWriter(f,['theme','url','img','lines','author'])
+#     w.writeheader()
+#     for quote in quotes:
+#         w.writerow(quote)
+
 
 class MinecraftHelperIntentHandler(AbstractRequestHandler):
   """Handler for minecraft helper intent"""

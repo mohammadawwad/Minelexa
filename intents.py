9bf61e05b7e6be64a054f6a1dbfcfd47f9d8ed01
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2021-08-20 05:22:56 +0000

    minimal progress

--- intents.py
+++ intents.py
@@ -1,38 +1,20 @@
 import ask_sdk_core.utils as ask_utils
 from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model.ui import SimpleCard
 import feedparser
 import requests
 from bs4 import BeautifulSoup
-import csv
+import webbrowser                                                             
 
 URL = "https://www.minecraftcraftingguide.net"
 r = requests.get(URL)
    
 soup = BeautifulSoup(r.content, 'html5lib')
 print(soup.prettify())
-   
-# quotes=[]  # a list to store quotes
-   
-# table = soup.find('div', attrs = {'id':'all_quotes'}) 
-   
-# for row in table.findAll('div',
-#                          attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
-#     quote = {}
-#     quote['theme'] = row.h5.text
-#     quote['url'] = row.a['href']
-#     quote['img'] = row.img['src']
-#     quote['lines'] = row.img['alt'].split(" #")[0]
-#     quote['author'] = row.img['alt'].split(" #")[1]
-#     quotes.append(quote)
-   
-# filename = 'inspirational_quotes.csv'
-# with open(filename, 'w', newline='') as f:
-#     w = csv.DictWriter(f,['theme','url','img','lines','author'])
-#     w.writeheader()
-#     for quote in quotes:
-#         w.writerow(quote)
 
+name = soup.find(id = 'boat') 
+print(name)
 
 class MinecraftHelperIntentHandler(AbstractRequestHandler):
   """Handler for minecraft helper intent"""
@@ -42,15 +24,22 @@ class MinecraftHelperIntentHandler(AbstractRequestHandler):
     
 
   def handle(self, handler_input):
-    feed = feedparser.parse('https://www.minecraftcraftingguide.net/')
     slots = handler_input.request_envelope.request.intent.slots
+    # interactionModel.languageModel.intents[].slots[].multipleValues.enabled
     item = slots['Item'].value
-    
-    speak_output = f'To craft that you will need {item}'
+
+    imgStart = 'https://www.minecraftcraftingguide.net/img/crafting/'
+    imgMid = item
+    imgEnd = '-crafting.png'
+    imgLink = imgStart + imgMid + imgEnd
+    print(imgLink)
+
+    speak_output = f'To craft that you will need {item} here is a link {imgLink}'
 
     return (
       handler_input.response_builder
           .speak(speak_output)
+          .set_card(SimpleCard('test', 'card_text'))#might need to link account for it to work
           .response
     )
 
